e8c67210f50d0c0623f84af968286db2ae18d2c3
karimmaassen <karim.maassen@gmail.com>
2013-03-29 15:21:37 +0100

    .cmd;

---
+++ snippets-clone.cmd
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+clear
+
+cd $HOME/AppData/Roaming/"Sublime Text 2"/Packages/User
+
+mkdir ssc
+
+cd ssc
+
+clear
+
+git clone git@bitbucket.org:SelfServiceCompany/sublime-text-snippets.git
+
+clear
+
+echo"  "
+echo"Done!"
