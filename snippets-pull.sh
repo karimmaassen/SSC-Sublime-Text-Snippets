b3e23085b67a89fbbf49bb726829cb9433f4d038
karimmaassen <karim.maassen@gmail.com>
2013-03-29 14:13:03 +0100

    Clone & Pull Shell Scripts;

---
+++ snippets-pull.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+clear
+
+cd $HOME/AppData/Roaming/"Sublime Text 2"/Packages/User/ssc/sublime-text-snippets
+
+clear
+
+git pull git@bitbucket.org:SelfServiceCompany/sublime-text-snippets.git
+
+clear
+
+echo" "
+echo"Done!"
